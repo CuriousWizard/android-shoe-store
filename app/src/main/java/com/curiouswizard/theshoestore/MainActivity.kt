c90e7898a7b834f13b43b566431faec7e28627e5
CuriousWizard <tremmel.david@hotmail.com>
2021-03-18 17:16:13 +0100

    Finished MainActivity

--- app/src/main/java/com/curiouswizard/theshoestore/MainActivity.kt
+++ app/src/main/java/com/curiouswizard/theshoestore/MainActivity.kt
@@ -2,12 +2,32 @@ package com.curiouswizard.theshoestore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.curiouswizard.theshoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+    lateinit var activityViewModel: ActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+
+        activityViewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
+        binding.lifecycleOwner = this
+
+        navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navHostFragment.navController.navigateUp()
     }
 }
