42c241801b36760bef615fe8f30f1f72773d7625
CuriousWizard <tremmel.david@hotmail.com>
2021-03-18 17:17:01 +0100

    Finished Details screen

---
+++ app/src/main/java/com/curiouswizard/theshoestore/ShoeDetailFragment.kt
@@ -0,0 +1,49 @@
+package com.curiouswizard.theshoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.curiouswizard.theshoestore.databinding.FragmentShoeDetailBinding
+import com.google.android.material.snackbar.Snackbar
+
+class ShoeDetailFragment : Fragment() {
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val binding: FragmentShoeDetailBinding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_detail, container, false)
+
+        val sharedViewModel: ActivityViewModel by activityViewModels()
+
+        binding.buttonSave.setOnClickListener {
+            if (binding.nameEditText.text.toString() != "" || binding.sizeEditText.text.toString() != "" ||
+                binding.companyEditText.text.toString() != "" || binding.descriptionEditText.text.toString() != "") {
+
+                    sharedViewModel.addShoe(
+                        binding.nameEditText.text.toString(),
+                        binding.sizeEditText.text.toString().toDouble(),
+                        binding.companyEditText.text.toString(),
+                        binding.descriptionEditText.text.toString()
+                )
+                findNavController().navigateUp()
+            }
+            else{
+                Snackbar.make(requireView(),R.string.empty_details, Snackbar.LENGTH_SHORT)
+                        .setAnchorView(R.id.button_save)
+                        .show()
+            }
+        }
+        binding.buttonCancel.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        return binding.root
+    }
+}
