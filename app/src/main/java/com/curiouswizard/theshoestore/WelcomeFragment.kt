812b31c1e2ef4b148d5d6e3a1c0241f52eeec0b9
CuriousWizard <tremmel.david@hotmail.com>
2021-03-18 17:13:02 +0100

    Finished Welcome screen

--- app/src/main/java/com/curiouswizard/theshoestore/WelcomeFragment.kt
+++ app/src/main/java/com/curiouswizard/theshoestore/WelcomeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.curiouswizard.theshoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,13 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val binding: FragmentWelcomeBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+        binding.fabNext.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionFragment())
+        }
+
+        return binding.root
     }
 }
