d1a1f565897b590844781014da84e52421234486
CuriousWizard <tremmel.david@hotmail.com>
2021-03-18 17:14:09 +0100

    Finished Instruction screen

--- app/src/main/java/com/curiouswizard/theshoestore/InstructionFragment.kt
+++ app/src/main/java/com/curiouswizard/theshoestore/InstructionFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.curiouswizard.theshoestore.databinding.FragmentInstructionBinding
 
 class InstructionFragment : Fragment() {
 
@@ -13,6 +16,13 @@ class InstructionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instruction, container, false)
+        val binding: FragmentInstructionBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_instruction, container, false)
+
+        binding.fabUnderstood.setOnClickListener {
+            findNavController().navigate(InstructionFragmentDirections.actionInstructionFragmentToShoeListFragment())
+        }
+
+        return binding.root
     }
 }
